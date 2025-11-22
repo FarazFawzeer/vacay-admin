b10e0fe27b7ee8a044aa8acf7091d6fe87c1bb1e
Faraz Fawzeer <farazfawzeer@gmail.com>
2025-11-22 13:53:10 +0530

    Ignore compiled Blade view files

--- storage/framework/views/4f42127235e9bcd2ca64b90c071722ca.php
+++
@@ -1,3 +0,0 @@
-<?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
-
-<?php echo $__env->yieldContent('scripts'); ?><?php /**PATH F:\Personal Projects\Vacay Guider\vacay-admin\resources\views/layouts/partials/vendor-scripts.blade.php ENDPATH**/ ?>
